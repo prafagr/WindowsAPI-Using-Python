4013b5b8d63bb6aba8f6ca6931af1efe825c68d8
prafagr <prafagr@gmail.com>
2020-07-30 07:28:21 +0530

    Create openprocesshandle.py

---
+++ openprocesshandle.py
@@ -0,0 +1,19 @@
+import ctypes
+
+k_handle = ctypes.WinDLL("Kernel32.dll")
+
+
+Process_All_Access = (0x000F0000 | 0x00100000 | 0xFFF)
+
+user_in = ctypes.c_ulong(int(input("Enter processID")))
+
+dwDesiredAccess = Process_All_Access
+bInheritHandle = False
+dwProcessId = user_in
+
+response = k_handle.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
+
+if response <= 0:
+    print("Handle was not obtained", k_handle.GetLastError())
+else:
+    print("Handle was obtained:",response)
